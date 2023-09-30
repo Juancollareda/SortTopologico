334f3f2154c57d3fd82b3e4dfc810e5b48cd078d
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2023-09-30 15:34:39 -0300

    modifique comprar y agregue funciones

--- tsort.py
+++ tsort.py
@@ -1,41 +1,51 @@
-from Grafo import Grafo  # Importa la clase Grafo del módulo Grafo
-from collections import deque  # Importa la clase deque de la biblioteca collections
-
+from Grafo import Grafo  
+from collections import deque  
+import yaml
 # Función para crear un grafo a partir de un diccionario de archivos y dependencias
 def crear_grafo_desde_diccionario(diccionario):
     grafo = Grafo()  # Crea una instancia de la clase Grafo
-
     # Agrega nodos y arcos al grafo basados en el diccionario
     for archivo, dependencias in diccionario.items():
         grafo.agregar_nodo(archivo)
         for destino in dependencias:
             grafo.agregar_nodo(destino)
             grafo.agregar_arco(archivo, destino)
-
     return grafo  # Devuelve el grafo creado
 def comparar_grafos(grafo_original, grafo_modificado):
     # Comparar nodos
-    nodos_originales = grafo_original.nodos
-    nodos_modificados = grafo_modificado.nodos
+    nodos_originales = set(grafo_original.nodos)
+    nodos_modificados = set(grafo_modificado.nodos)
     nodos_nuevos = nodos_modificados - nodos_originales
     nodos_eliminados = nodos_originales - nodos_modificados
-
     # Comparar arcos
-    arcos_originales = grafo_original.arcos
-    arcos_modificados = grafo_modificado.arcos
-    # Realiza la comparación de arcos según tus necesidades
-
-    # Puedes personalizar esta función de comparación según lo que desees rastrear en tu grafo
-
-    if nodos_nuevos or nodos_eliminados:
-        print("Se han realizado cambios en los nodos del grafo:")
-        print("Nodos Nuevos:", nodos_nuevos)
-        print("Nodos Eliminados:", nodos_eliminados)
+    arcos_originales = set(grafo_original.arcos)
+    arcos_modificados = set(grafo_modificado.arcos)
+    arcos_nuevos = arcos_modificados - arcos_originales
+    arcos_eliminados = arcos_originales - arcos_modificados
+    if nodos_nuevos or nodos_eliminados or arcos_nuevos or arcos_eliminados:
+        print("Se han realizado cambios en el grafo:")
+        if nodos_nuevos or nodos_eliminados:
+            print("Cambios en nodos:")
+            print("Nodos Nuevos:", nodos_nuevos)
+            print("Nodos Eliminados:", nodos_eliminados)
+        if arcos_nuevos or arcos_eliminados:
+            print("Cambios en arcos:")
+            print("Arcos Nuevos:", arcos_nuevos)
+            print("Arcos Eliminados:", arcos_eliminados)
     else:
-        print("No se han realizado cambios en los nodos del grafo.")
+        print("No se han realizado cambios en el grafo.")
 
-    # Puedes agregar lógica adicional para comparar arcos u otros aspectos del grafo
-
+def cargar_yaml_a_dict(ruta_archivo):
+    try:
+        with open(ruta_archivo, "r") as archivo:
+            datos = yaml.safe_load(archivo)
+        return datos
+    except FileNotFoundError:
+        print(f"El archivo '{ruta_archivo}' no fue encontrado.")
+        return None
+    except Exception as e:
+        print(f"Se produjo un error al cargar el archivo YAML: {str(e)}")
+        return None 
 
 # Función para realizar una ordenación topológica utilizando BFS
 def tsort(grafo):
@@ -65,22 +75,28 @@ def tsort(grafo):
     else:
         return None  # Devuelve None si el grafo contiene un ciclo
     #esa parte del código que verifica si la longitud del orden topológico es igual a la cantidad de nodos en el grafo es una forma común de determinar si el grafo contiene un ciclo.
-
-
-
 # Función principal
 def main():
     archivos = {
         "a": ["b", "c"],
-        "b": ["a", "e"],
+        "b": ["g"],
         "c": ["f", "g"],
-        "g": ["h"]
+        "g": ["h"],
+        "h": ["f"],
+        "f": [],
     }
-    
-    mi_grafo = crear_grafo_desde_diccionario(archivos)
-    
-    orden_topologico = tsort(mi_grafo)
-    
+    archivos2 = {
+        "a": ["b"],
+        "b": ["g"],
+        "c": ["f", "g"],
+        "g": ["h"],
+        "h": []
+    }
+    dict3=cargar_yaml_a_dict('archivo.yaml')
+    mi_grafo = crear_grafo_desde_diccionario(dict3)
+    #mi_grafo2 = crear_grafo_desde_diccionario(archivos2)
+   # comparar_grafos(mi_grafo,mi_grafo2)
+    orden_topologico = tsort(mi_grafo)    
     if orden_topologico:
         print("Secuencia de archivos para el linker:")
         for archivo in orden_topologico:
@@ -89,6 +105,6 @@ def main():
         print("El grafo contiene un ciclo.")
 
 if __name__ == "__main__":
-    main()  # Llama a la función principal si este script se ejecuta directamente
+    main()  
 
 
